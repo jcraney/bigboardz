077e08222944e4cabebca758e221609c6ee58e3b
jcraney <jcraney@gmail.com>
2025-04-22 20:04:40 +0000

    added angular material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { BoardListComponent } from './board-list/board-list.component';
 import { WaitingListComponent } from './waiting-list/waiting-list.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/boardz', pathMatch: 'full' },
   { path: 'boardz', component: BoardListComponent },
   { path: 'waiting-list', component: WaitingListComponent}
 ];
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent { }
+export class NavBarComponent { 
+
+}
